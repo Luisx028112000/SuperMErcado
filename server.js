4be29329b12ed9435d7244c2e9c8be51d45c3a5d
Luisx028112000 <lapara1928@gmail.com>
2024-11-03 12:26:36 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,100 @@
+
+
+import express from "express";
+import mysql from "mysql2";
+import { z } from "zod";
+
+//* App
+const app = express()
+const port = 3000; //? 3000
+
+app.use(express.static('src'));
+app.use(express.json());
+
+//* Config Db
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "8293716541@Ll",
+    database: "superMercado"
+})
+
+db.connect(err => {
+    if (err) {
+        console.log(`Error ${err}`);
+        return
+    }
+
+    console.info("Conexion exitosa")
+})
+
+//* node --watch server.js
+
+const articleSchema = z.object({
+    nombre: z.string().min(1),
+    descripcion: z.string().optional(),
+    precio: z.number().nonnegative(),
+    cantidad: z.number().int().nonnegative(),
+    categoria: z.string().optional(),
+    image: z.string().url().optional()
+})
+
+//* API Route
+
+app.get("/product", (req, res) => {
+    db.query(`select * from productos`, (error, result) => {
+        if (error) {
+            return res.status(500).json({ error: err.message })
+        }
+        return res.json(result);
+    })
+});
+
+    // const body = document.querySelector("body");
+    // app.get("/", (red, res) => 
+    //     body.innerText = "Nada"
+    // })
+
+app.get("/product/search", (req, res) => {
+
+    const { nombre, categoria } = req.query;
+
+    let query = `select * from supermercado.productos where `
+
+    const params = [];
+
+    if (nombre) {
+        query += `nombre = "${nombre}"`;
+        // params.push(`${nombre}`)
+
+    }
+    if (categoria) {
+        query += `categoria = "${categoria}"`;
+        // params.push(`${categoria}`)
+    }
+
+
+    db.query(query, (error, result) => {
+
+        console.log(query)
+
+        if (error) {
+            return res.status(500).json({ error: error.message })
+        }
+        return res.json(result);
+    })
+});
+
+
+
+//* app.post();
+
+//? app.delete();
+
+//TODO app.put();
+
+    
+app.listen(port, () => {
+    console.log(`Server is running http:localhost:${port}`)
+})
